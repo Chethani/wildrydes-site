5370e8d428f71b0edbf43fc06e52175b8b7f58ee
Chethani <30625637+Chethani@users.noreply.github.com>
2026-02-20 18:08:58 +0530

    Update userPoolClientId in config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_pcvToPlbz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '30msup7vo49cpj5fe3p48ttlhg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6e5onhhcii2j1vt6t5ninrprmc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
